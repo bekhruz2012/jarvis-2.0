d579ea8d904b4ff61696274ab4c49a479a403b9c
Bekhruz <buribaevbehruz334@gmail.com>
2026-08-29 00:08:27 +0500

    Add Vercel deployment

---
+++ api/index.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(
+            b'{"status":"online","service":"JARVIS 2.0"}'
+        )
